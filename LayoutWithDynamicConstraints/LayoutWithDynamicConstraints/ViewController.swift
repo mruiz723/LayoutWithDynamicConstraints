848f276bfc5600ecc4c5095430362f99279c8285
mruiz723 <mruiz723@gmail.com>
2017-07-23 20:40:40 -0500

    Add mapView, webView, contentView with two textView. These elements have the constraints for portrait view

--- LayoutWithDynamicConstraints/LayoutWithDynamicConstraints/ViewController.swift
+++ LayoutWithDynamicConstraints/LayoutWithDynamicConstraints/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    //MARK: - Properties
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadWebView()
+        
+        let initialLocation = CLLocation(latitude: 6.27053, longitude: -75.57211999999998)
+        centerMapOnLocation(location: initialLocation)
+            
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func centerMapOnLocation(location: CLLocation) {
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(6.27053, -75.57211999999998)
+        annotation.title = "Medellín"
+        
+        mapView.addAnnotation(annotation)
+        
+        let regionRadius: CLLocationDistance = 1000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        let adjustedRegion = mapView.regionThatFits(coordinateRegion)
+        mapView.setRegion(adjustedRegion, animated: true)
     }
 
-
+    
+    //MARK: - Utils
+    func loadWebView() {
+        let url = URL(string: "https://stackoverflow.com")
+        let request = URLRequest.init(url: url!)
+        webView.loadRequest(request)
+    }
+    
+    
+    //MARK: - UIWebViewDelegate
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print("Error webview: ", error.localizedDescription)
+    }
+    
 }
 
